af042ad1d8f350c4ddeeaa3c180017ef0d92f3d5
beckjosh <Joshbeck17@gmail.com>
2024-07-09 03:01:24 +0200

    "Created functionality for next/previous & select"

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,61 @@
+import React, { useState } from "react";
 import "./App.css";
 import QuestionComponent from "./components/question";
-import questionItems from "./questions.json"; 
+import questionItems from "./questions.json";
 
 const App: React.FC = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const handleNextQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) =>
+      prevIndex < questionItems.length - 1 ? prevIndex + 1 : prevIndex
+    );
+  };
+
+  const handlePrevQuestion = () => {
+    setCurrentQuestionIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : prevIndex
+    );
+  };
+
+  const handleSelectQuestion = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setCurrentQuestionIndex(Number(event.target.value));
+  };
+
+  const currentQuestion = questionItems[currentQuestionIndex];
+
   return (
     <>
-      {questionItems.map((question, index) => (
-        <QuestionComponent
-          key={index}
-          index={index}
-          question={question.question}
-          answers={question.answers}
-          correctAnswer={question.correct_answer}
-        />
-      ))}
+      <div>
+        <button
+          onClick={handlePrevQuestion}
+          disabled={currentQuestionIndex === 0}
+        >
+          Previous Question
+        </button>
+        <button
+          onClick={handleNextQuestion}
+          disabled={currentQuestionIndex === questionItems.length - 1}
+        >
+          Next Question
+        </button>
+        <select onChange={handleSelectQuestion} value={currentQuestionIndex}>
+          {questionItems.map((_, index) => (
+            <option key={index} value={index}>
+              Question {index + 1}
+            </option>
+          ))}
+        </select>
+      </div>
+      <QuestionComponent
+        key={currentQuestionIndex}
+        index={currentQuestionIndex}
+        question={currentQuestion.question}
+        answers={currentQuestion.answers}
+        correctAnswer={currentQuestion.correct_answer}
+      />
     </>
   );
 };
