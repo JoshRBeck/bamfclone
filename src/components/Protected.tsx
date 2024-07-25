96f2cb15dfdb4bfb6722ec3ab4dcd4abd907f4ad
beckjosh <Joshbeck17@gmail.com>
2024-07-25 03:57:49 +0200

    "Added tailwind, but not fully functioning yet"

--- src/components/Protected.tsx
+++ src/components/Protected.tsx
@@ -2,35 +2,33 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Protected: React.FC = () => {
-  const [data, setData] = useState<string>('');
+  const [data, setData] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         if (!token) {
-          alert('No token found. Please log in.');
+          alert("No token found. Please log in.");
           return;
         }
-        
-        const response = await axios.get('http://localhost:5173/protected', {
+
+        const response = await axios.get("http://localhost:5173/protected", {
           headers: { Authorization: `Bearer ${token}` },
         });
-        
-        setData(response.data.message || 'Protected data fetched successfully.');
+
+        setData(
+          response.data.message || "Protected data fetched successfully."
+        );
       } catch (err) {
-        alert('Failed to fetch protected data');
+        alert("Failed to fetch protected data");
       }
     };
 
     fetchData();
   }, []);
 
-  return (
-    <div>
-      {data}
-    </div>
-  );
-}
+  return <div>{data}</div>;
+};
 
 export default Protected;
